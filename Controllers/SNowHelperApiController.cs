b5bf9345d59c7bd05066aa673a01dabc01dd499f
Borah <moushom.borah@accenture.com>
2021-07-29 19:27:31 +0530

    aadded SNOW API controller

---
+++ Controllers/SNowHelperApiController.cs
@@ -0,0 +1,47 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TrueDynamicWeb.Models;
+
+namespace TrueDynamicWeb.Controllers
+{
+    public class SNowHelperApiController : ApiController
+    {
+        // GET: api/SNowHelperApi
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SNowHelperApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/SNowHelperApi
+        public IHttpActionResult Post(Url_config_tabl UrlConfigtabl)
+        {
+            var client = new RestClient(UrlConfigtabl.url);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("authorization", "Basic YXRtb3Myc25vdzpGUiU4Sm13cFU5NTZw");
+            IRestResponse response = client.Execute(request);
+            return Ok(response);
+        }
+
+        // PUT: api/SNowHelperApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SNowHelperApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
